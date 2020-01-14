a6e78f378638d062946c771d00ced8b880b57970
Eric Dobbertin <eric@dairystatedesigns.com>
2020-01-14 10:46:42 -0600

    fix: ensure fix-volumes uses node user on Mac
    Using `stat -c "%u:%g" .` to determine the UID:GID does
    not work when the host machine is Mac OSX and the
    working directory (`.`) is itself a volume linked to the
    host machine. On Mac, the command will always show
    root (0:0) as the owner because all volumes are always
    owned by root. This is true even if the underlying image
    has this directory pre-created with different ownership.
    The volume ownership masks the ownership built into
    the image.
    
    Since the true purpose of `fix-volumes.sh` is to ensure
    that all volumes are owned by the same user who will
    run the command, my fix is to pass this user name into
    the script as an argument. Currently we could assume
    "node" user, but the script is more versatile if it accepts
    the user name as an argument because only the calling
    Dockerfile/entrypoint truly knows which user it will be
    running the command as.
    
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- scripts/fix-volumes.sh
+++ scripts/fix-volumes.sh
@@ -13,26 +13,35 @@ set -o posix    # more strict failures in subshells
 IFS=$'\n\t'
 # ---- End unofficial bash strict mode boilerplate
 
+if [[ "$#" -ne 1 ]]; then
+  echo "Usage: $0 [desired_owner_user]" 1>&2
+  exit 1
+fi
+
 volumes=$(mount | grep -E '^/dev/' | grep -Ev ' on /etc/' || true)
 if [[ -z "${volumes}" ]]; then
-  exit
-fi
-owner=$(stat -c "%u:%g" .)
-if [[ "${owner}" =~ ^0: ]]; then
-  echo "$(basename "$0") aborting instead of chowning to root."
-  echo "Found owner ${owner} on ${PWD}."
+  echo "fix-volumes: No volumes found"
   exit
 fi
 
+desired_owner_user=$1
+owner="$(id -u ${desired_owner_user}):$(id -g ${desired_owner_user})"
+if [[ "${owner}" =~ ^0: ]]; then
+  echo "fix-volumes: $(basename "$0") aborting instead of chowning to root."
+  echo "fix-volumes: Found owner ${owner} on ${PWD}."
+  exit
+fi
+
+echo "fix-volumes: Fixing all volumes to be owned by user '${desired_owner_user}''"
 echo "${volumes}" | awk '{print $3}' | {
   while IFS= read -r dir; do
-    echo "${dir}"
     mkdir -p "${dir}"
     old_owner=$(stat -c "%u:%g" "${dir}")
     if [[ "$1" != "--force" && "${old_owner}" == "${owner}" ]]; then
+      echo "fix-volumes: Skipping volume ${dir}. Already owned by ${owner}"
       continue
     fi
-    printf "Fixing volume %s (before=%s after=%s)…" "${dir}" "${old_owner}" "${owner}"
+    printf "fix-volumes: Fixing volume %s (before=%s after=%s)…" "${dir}" "${old_owner}" "${owner}"
     chown -R "${owner}" "${dir}"
     chmod -R a+r,u+rw "${dir}"
     echo "✓"
